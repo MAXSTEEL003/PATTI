4fb2dac584073218a42bac94ac9908a596ee8088
MAXSTEEL003 <tejasadinarayan@gmail.com>
2025-06-01 00:20:38 +0530

    Initial commit: Bill Generator application

---
+++ terminal-commands.sh
@@ -0,0 +1,7 @@
+# Link your local repository to GitHub
+git remote add origin https://github.com/MAXSTEEL003/PATTIF
+
+# Push your code to GitHub
+git push -u origin main
+# If the above command fails, try:
+# git push -u origin master
